176d72dba745193edbb741784c457b97b63e5c7a
Undeadprogramming <95182800+Undeadprogramming@users.noreply.github.com>
2024-04-21 01:45:45 +0200

    bez double priv

--- signature.py
+++ signature.py
@@ -1,87 +1,26 @@
-import PyPDF2
-import cryptography
-from pdfrw import PdfReader, PdfWriter 
+from datetime import datetime
 import hashlib
+from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives.asymmetric import padding
 import os
-
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        for page in reader.pages:
-            text += page.extract_text()
-    return text
-
-def extract_text_from_txt(txt_path):
-    try:
-        with open(txt_path, "r") as file:
-            text = file.read()
-    except FileNotFoundError:
-        print("File not found.")
-    except Exception as e:
-        print("An error occurred:", e)
-    return text
-
-def extract_signed_text_from_txt(txt_path):
-    try:
-        with open(txt_path, "r") as file:
-            text = file.read()
-            signature_start = text.find('\n\n-----BEGIN SIGNATURE-----\n')
-            text = text[:signature_start]
-    except FileNotFoundError:
-        print("File not found.")
-    except Exception as e:
-        print("An error occurred:", e)
-    return text
-
-
-# Function to extract ciphertext from the PDF metadata
-def extract_ciphertext_from_pdf(pdf_path):
-    trailer = PdfReader(pdf_path)
-    ciphertext_hex = trailer.Info.hash[1:][:-1]
-    print("Cipher:",ciphertext_hex)
-
-    try:
-        # Ensure the string is in the correct format (e.g., remove any leading or trailing whitespace)
-        ciphertext_hex = ciphertext_hex.strip()
-
-        # Convert hexadecimal string back to bytes
-        ciphertext = bytes.fromhex(ciphertext_hex)
-        return ciphertext
-    except ValueError as e:
-        print("Error extracting ciphertext:", e)
-        print("Signature verification failed!")
-        exit (1)
-
-
-def extract_public_key_from_pdf(pdf_path):
-    try:
-        # Read the PDF file and load its metadata
-        trailer = PdfReader(pdf_path)
-        # Access the custom field containing the public key
-        public_key_str = trailer.Info.public_key[1:][:-1]
-        #public_key_str = public_key_str.replace('\\n', '\n') 
-        print(public_key_str)
-        
-        # Decode the public key string
-        public_key_pem = public_key_str.encode('utf-8')
-        
-        # Deserialize the public key from PEM format
-        public_key = serialization.load_pem_public_key(public_key_pem, backend=default_backend())
-
-        return public_key
-    except Exception as e:
-        print("Error extracting public key:", e)
-        print("Signature verification failed!")
-        exit (1)
-
+import re
 
 # Generate RSA keys
+
+def extract_rsa_private(key):
+    n = key.private_numbers().p * key.private_numbers().q
+    d = key.private_numbers().d
+    return n, d
+
+def extract_rsa_public(key):
+    n = key.public_numbers().n
+    e = key.public_numbers().e
+    return n, e
+
 def key_gen():
     private_key = rsa.generate_private_key(
     public_exponent=65537,
@@ -101,158 +40,281 @@ def str_key(public_key):
     public_key_str = public_key_pem.decode('utf-8')
     return public_key_str
 
-def sign(private_key,hash_value):
-    # Encrypt the hash value with the public key
-    ciphertext = private_key.sign(
-    hash_value,
-    padding.PSS(
-        mgf=padding.MGF1(hashes.SHA256()),
-        salt_length=padding.PSS.MAX_LENGTH
-    ),
-    hashes.SHA256()
+def str_keys(private_key):
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
     )
-    return ciphertext
-
-def verify(public_key,ciphertext,hash_value):
-    public_key.verify(
-        ciphertext,
-        hash_value,
-        padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
-        ),
-        hashes.SHA256()
-    )
+    private_key_str = private_key_pem.decode('utf-8')
+    return private_key_str
     
-def save_signature_and_key(signature, public_key, txt_path):
-    with open(txt_path, 'a') as file:
-        file.write('\n\n-----BEGIN SIGNATURE-----\n')
-        file.write(signature.hex())
-        file.write('\n-----END SIGNATURE-----\n')
-        file.write('\n-----BEGIN PUBLIC KEY-----\n')
-        file.write(str_key(public_key))
-        file.write('\n-----END PUBLIC KEY-----\n')
 
-def extract_signature_and_key(txt_content):
+def my_sign(private_key,hash_value):
+    # Encrypt the hash value with the public key
+    n_private, d = extract_rsa_private(private_key)
+    ciphertext = pow(int.from_bytes(hash_value,'big'),d,n_private)
+    return hex(ciphertext)[2:]
+
+def my_verify(public_key,signature,hash_value):
+    n_public, e = extract_rsa_public(public_key)
+    new_hash = pow(int(signature,16),e,n_public)
+    
+    if(int.from_bytes(hash_value,'big') == new_hash):
+        print("Signature verified successfully!")
+    else:
+        print("Signature verification FAILED!")
+
+def my_hash(content):
+    hash_algorithm = hashes.SHA256()
+    hasher = hashes.Hash(hash_algorithm,default_backend())
+    hasher.update(content.encode('utf-8'))
+    hash_value = hasher.finalize()
+    return hash_value
+
+def generate_certificate():
+    private_key, public_key = key_gen()
+    public_test = open("public.pem", "w")
+    public_test.write(str_key(public_key))
+    subject = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+        x509.NameAttribute(NameOID.COMMON_NAME, u"example.com"),
+    ])
+
+        # Set the expiration date for the certificate
+    expiration_date = datetime(2025, 12, 31)  # Set expiration to December 31, 2025
+
+    # Create a self-signed certificate
+    certificate = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        subject
+    ).public_key(
+        public_key
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.utcnow()
+    ).not_valid_after(
+        expiration_date
+    ).sign(
+        private_key, hashes.SHA256(), default_backend()
+    )
+
+    # Export the private key and certificate to files
+    crt_input = input("Enter certificate filename:\n").split('.')
+    crt_file= crt_input[0]
+    
+    with open(crt_file +"_private_key.pem", "w") as private_key_file:
+       
+        private_key_file.write(str_keys(private_key))
+       
+    print("Private key saved as:"+crt_file +"_private_key.pem")
+    
+    with open(crt_file +".pem", "wb") as certificate_file:
+        certificate_file.write(certificate.public_bytes(serialization.Encoding.PEM))
+        
+    print("Certificate saved as:"+crt_file +".pem")
+
+
+    return private_key, certificate, crt_file+'.pem'
+
+def extract_certificate(crt_name):
+    with open(crt_name, "rb") as cert_file:
+        cert_data = cert_file.read()
+
+    # Parse the certificate
+    certificate = x509.load_pem_x509_certificate(cert_data, default_backend())
+
+    # Extract information from the certificate
+    # print("Subject:")
+    # print(certificate.subject)
+
+    # print("\nIssuer:")
+    # print(certificate.issuer)
+
+    # print("\nSerial Number:")
+    # print(certificate.serial_number)
+
+    # print("\nNot Valid Before:")
+    # print(certificate.not_valid_before)
+
+    # print("\nNot Valid After:")
+    # print(certificate.not_valid_after)
+
+    # print("\nPublic Key:")
+    # print(str_key(certificate.public_key()))
+
+    return certificate.public_key()
+
+def verify_certificate(cert_path):
+    # Load the certificate
+    with open(cert_path, "rb") as cert_file:
+        cert_data = cert_file.read()
+        
+    cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+
+    # Verify the certificate signature using trusted certificates
+    try:
+        cert.public_key().verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        print("Certificate signature verified successfully.")
+    except Exception as e:
+        print("Certificate signature verification failed!", e)
+        return False
+
+    # Additional verification checks can be added here (e.g., expiry, revocation, chain validation)
+
+    return True
+    
+def save_signature(signature, file):  
+        file.write('-----BEGIN SIGNATURE-----\n')
+        file.write(str(signature))
+        file.write('\n-----END SIGNATURE-----\n')
+        
+def save_certificate_key(public_key, file):  
+        file.write(str_keys(public_key))
+
+def save_certificate(certificate, file):  
+        file.write('-----BEGIN CERTIFICATE-----\n')
+        #file.write(re.sub("(.{64})", "\\1\n", certificate, 0, re.DOTALL))
+        file.write(certificate)
+        file.write('\n-----END CERTIFICATE-----\n')
+
+
+def extract_signature(txt_content):
     signature_start = txt_content.find('-----BEGIN SIGNATURE-----')
     signature_end = txt_content.find('-----END SIGNATURE-----')
-    public_key_start = txt_content.find('-----BEGIN PUBLIC KEY-----')
-    public_key_end = txt_content.find('-----END PUBLIC KEY-----')
 
-    if signature_start != -1 and signature_end != -1 and public_key_start != -1 and public_key_end != -1:
+    if signature_start != -1 and signature_end != -1:
         try:
-            signature = bytes.fromhex(txt_content[signature_start + len('-----BEGIN SIGNATURE-----'):signature_end].strip())
+            signature = hex(int(txt_content[signature_start + len('-----BEGIN SIGNATURE-----'):signature_end].strip('\n'),16))
         except Exception as e:
             print("Error loading signature:", e)
             print("Signature verification failed!")
             exit(1)
-        # Extract the entire public key substring
-        public_key_str = (txt_content[public_key_start + len('-----BEGIN PUBLIC KEY-----'):public_key_end]+'-----END PUBLIC KEY-----').strip()
-
         try:
-            public_key = serialization.load_pem_public_key(public_key_str.encode('utf-8'), backend=default_backend())
-            return signature, public_key
+            return signature[2:]
         except Exception as e:
-            print("Error loading public key:", e)
+            print("Error loading signature:", e)
             print("Signature verification failed!")
             exit(1)
+
     else:
         print("Signature and/or public key not found in the text file.")  # Debugging print statement
-        return None, None
-
-
-  
+        return None
 
 #####################________MAIN_FUNCTION_________###################
 
-# Path to the original PDF
-pdf_path = 'lorem.pdf'
-txt_path = 'lorem.txt'
-
-choice = input("Choose action:\nPress '1' for signing\nPress '2' for verification\n")
-
-if(choice == "1"):
-    format = input("Choose file format:\nPress '1' for pdf\nPress '2' for txt\n")
-    if(format == "1"):
-        print("Generating keys...")
-        private_key, public_key=key_gen()
+exit_program = False
+while(not exit_program):
+    choice = input("Choose action:\nPress '1' for creating a certificate\nPress '2' for signing\nPress '3' for verification\nPress '4' to exit\n")
     
-        print("Creating hash...")
-        # Extract text from the PDF
-        pdf_text = extract_text_from_pdf(pdf_path)
-        # Hash the text
-        hash_value = hashlib.sha256(pdf_text.encode('utf-8')).digest()
+    if(choice == "1"):
+        CA_private_key, certificate,crt_name = generate_certificate()
+        extract_certificate(crt_name)
 
-        print("Signing PDF...")
-        ciphertext = sign(private_key,hash_value)
-
-        # Embed the ciphertext as metadata in the PDF file
-        trailer = PdfReader(pdf_path)
-        trailer.Info.hash = ciphertext.hex()  # Store the ciphertext as hexadecimal string
-        trailer.Info.public_key = str_key(public_key)
-        PdfWriter(pdf_path, trailer=trailer).write()
-        print("PDF signed successfully!")
-    if(format == "2"):
-        print("Generating keys...")
-        private_key, public_key=key_gen()
-    
-        print("Creating hash...")
-        txt_text = extract_text_from_txt(txt_path)
-        if(txt_text.find('\n\n-----BEGIN SIGNATURE-----\n') != -1):
-            txt_text = extract_signed_text_from_txt(txt_path)
-            f = open(txt_path, 'r+')
-            f.truncate(0)
-            f.write(txt_text)
-            print("Previous signature replaced...")
+    if(choice == "2"):
+        crt_name = input("Enter certificate name:\n").split('.')
+        crt_path = crt_name[0] + '.pem'
+        private_path = crt_name[0] + '_private_key.pem'
+        file_path = input("Choose file:\n")
+        file_path_split= file_path.split('.')
+        txt_key = file_path_split[0] + '.pem' 
         
-        if txt_text:
-            # Hash the text
-            hash_value = hashlib.sha256(txt_text.encode('utf-8')).digest()
-            print("Signing txt...")
-            signature = sign(private_key, hash_value)
-
-            # Save signature and public key
-            save_signature_and_key(signature, public_key, txt_path)
-            print("Text file signed successfully!")
-    
-if(choice == "2"):
-    format = input("Choose file format:\nPress '1' for pdf\nPress '2' for txt\n")
-    if(format == "1"):
-
-        print("Extracting key and hash from pdf...")
-        public_key = extract_public_key_from_pdf(pdf_path)
-        # Read the PDF file and load its metadata
-        ciphertext = extract_ciphertext_from_pdf(pdf_path)
-    
-        print("Hashing PDF contents...")
-        # Extract text from the PDF
-        pdf_text = extract_text_from_pdf(pdf_path)
-        # Hash the text
-        hash_value = hashlib.sha256(pdf_text.encode('utf-8')).digest()
-        
-        # Decrypt the ciphertext with the private key
-        print("Verifying signature...")
-        try:
-            verify(public_key,ciphertext,hash_value)
-            print("Signature verified successfully!")
-        except cryptography.exceptions.InvalidSignature:
-            print("Signature verification failed!")
-    if(format == "2"):
-
-        txt_text = extract_text_from_txt(txt_path)
-        if txt_text:
-            signature, public_key = extract_signature_and_key(txt_text)
-            if signature and public_key:
-                #Hash the text
-                print("Creating hash...")
-                txt_text = extract_signed_text_from_txt(txt_path)
-                hash_value = hashlib.sha256(txt_text.encode('utf-8')).digest()
+        if(os.path.isfile(crt_path)):
+            with open(crt_path,'r+') as data_crt:
+                crt_content = data_crt.read()
+        else:
+            print("Certificate file doesnt exist.")
+            exit()
+            
+        if(os.path.isfile(private_path)):
+            with open(private_path,'r+') as data_private:
+                private_content = data_private.read()
                 
+            try:
+                private_key = serialization.load_pem_private_key(private_content.encode('utf-8'),password = None, backend=default_backend())
+            
+            except Exception as e:
+                print("Error loading private key:", e)
+                print("Signature verification failed!")
+                exit(1)
+        else:
+            print("Private key file doesnt exist.")
+            exit()
+        
+        if(os.path.isfile(file_path)):
+            with open(file_path,'r+') as data_txt:
+                file_content = data_txt.read()
+        else:
+            print("Data file doesnt exist.")
+            exit()    
+        
+        print("Creating hash...")    
+        with open("data.pem",'w') as signature_file:
+            hash_value = my_hash(file_content)
+            
+            print("Signing txt...")
+            signature = my_sign(private_key, hash_value)
+            
+            save_signature(signature, signature_file)
+            print("Text file signed successfully!")
+        
+    if(choice == "3"):
+        crt_name = input("Enter certificate name:\n").split('.')
+        crt_path = crt_name[0] + '.pem'
+        file_path = input("Choose file:\n")
+        file_path_split= file_path.split('.')
+        txt_key= file_path_split[0] + '.pem'   
+        
+        if(os.path.isfile(crt_path)):
+            with open(crt_path,'r+') as data_crt:
+                crt_content = data_crt.read()
+        else:
+            print("Certificate file doesnt exist.")
+            exit()
+        
+        if(os.path.isfile(txt_key)):
+            with open(txt_key,'r+') as data_txt:
+                file_content = data_txt.read()
+        else:
+            print("Data file doesnt exist.")
+            exit() 
+            
+        if(os.path.isfile(file_path)):
+            with open(file_path,'r+') as file_orig:
+                orig_content = file_orig.read()
+        else:
+            print("Data file doesnt exist.")
+            exit()   
+            
+        public_key = extract_certificate(crt_path)
+        signature = extract_signature(file_content)
 
-                # Verify signature
-                print("Verifying signature...")
-                try:
-                    verify(public_key,signature,hash_value)
-                    print("Signature verified successfully!")
-                except cryptography.exceptions.InvalidSignature:
-                    print("Signature verification failed!")
+        #Hash the text
+        print("Creating hash...")            
+                
+        hash_value = my_hash(orig_content)
+        #signature2 = sign(private_key, hash_value)
+        # Verify signature
+        print("Verifying signature...")
+        verify_certificate(crt_path)
+        my_verify(public_key,signature,hash_value)
+        
+    
+
+    if(choice == "4"):
+        exit_program = True
+
+    
+                
+                
+             
